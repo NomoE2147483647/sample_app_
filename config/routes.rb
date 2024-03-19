46e19dd4bf07438d6d341f52146e67b03e286c0f
NomoE <55677038+NomoE2147483647@users.noreply.github.com>
2024-03-19 16:32:18 +0000

    20240320 create form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
   get "/about",   to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/signup",  to: "users#new"
+  resources :users
 end
+
+#resources :usersを追加することで使えるようになる名前付きルーティング
+#GET	  /users	      index	  users_path	          すべてのユーザーを一覧するページ
+#GET	  /users/1	    show	  user_path(user)	      特定のユーザーを表示するページ
+#GET	  /users/new	  new	    new_user_path       	ユーザーを新規作成するページ（ユーザー登録）
+#POST	  /users	      create	users_path	          ユーザーを作成するアクション
+#GET	  /users/1/edit	edit	  edit_user_path(user)	id=1のユーザーを編集するページ
+#PATCH	/users/1	    update	user_path(user)	      ユーザーを更新するアクション
+#DELETE	/users/1	    destroy	user_path(user)     	ユーザーを削除するアクション
