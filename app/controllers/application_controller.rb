d1597fd70e46d6f0382e957faa6429faa58ebfd1
NomoE <55677038+NomoE2147483647@users.noreply.github.com>
2024-03-30 15:03:47 +0000

    20240331commit ~List 8.24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello,world"
-  end
+    include SessionsHelper
 end
+    #↑ApplicationControllerにSessionsHelperをincludeで読み込ませている
+    #ログイン機構はあらゆる場面で使うことになるので、全コントローラの親であるApplicationControllerに読み込ませている
+    #こうすることでApplicationControllerを継承している全コントローラでSessionsHelperの使用が可能になる。
+    #[予備知識]Helperファイル自体は暗黙的にObjectクラスを継承しているだけで特に他のクラスを継承していない
+
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-   render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session  #ログインの直前に必ずこれを書くこと。セッション固定攻撃への対策になる。
+      log_in user
+      redirect_to user
+      #user_url(user)のプロフィールページへの変換
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
